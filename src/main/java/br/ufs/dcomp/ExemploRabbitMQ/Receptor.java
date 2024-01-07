5299c7f531ce5d2fa8b89ad74c7e4c9a666486e6
r00drigues123 <133833002+r00drigues123@users.noreply.github.com>
2024-01-07 18:49:59 -0300

    Update Receptor.java

--- src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
+++ src/main/java/br/ufs/dcomp/ExemploRabbitMQ/Receptor.java
@@ -4,35 +4,90 @@ import com.rabbitmq.client.*;
 
 import java.io.IOException;
 
+import java.util.Scanner;
+
+import java.time.LocalDate;
+
+import java.time.LocalTime;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
 public class Receptor {
 
-  private final static String QUEUE_NAME = "minha-fila";
+  //private static String user = "";
 
   public static void main(String[] argv) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+    
     ConnectionFactory factory = new ConnectionFactory();
-    factory.setHost("ip-da-instancia-da-aws"); // Alterar
-    factory.setUsername("usuário-do-rabbitmq-server"); // Alterar
-    factory.setPassword("senha-do-rabbitmq-server"); // Alterar
+    factory.setHost("ec2-54-145-245-115.compute-1.amazonaws.com"); 
+    factory.setUsername("admin");
+    factory.setPassword("password");
     factory.setVirtualHost("/");   
     Connection connection = factory.newConnection();
     Channel channel = connection.createChannel();
+    
+    System.out.print("User: ");
+    String user = scanner.nextLine();
 
                       //(queue-name, durable, exclusive, auto-delete, params); 
-    channel.queueDeclare(QUEUE_NAME, false,   false,     false,       null);
+    channel.queueDeclare(user, false,   false,     false,       null);
     
-    System.out.println(" [*] Esperando recebimento de mensagens...");
-
+    
     Consumer consumer = new DefaultConsumer(channel) {
       public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)           throws IOException {
-
+        
+        Object sender = properties.getHeaders().get("sender");
+        Object date = properties.getHeaders().get("date");
+        Object hour = properties.getHeaders().get("hour");
+        
         String message = new String(body, "UTF-8");
-        System.out.println(" [x] Mensagem recebida: '" + message + "'");
-
-                        //(deliveryTag,               multiple);
-        //channel.basicAck(envelope.getDeliveryTag(), false);
+        System.out.println("");
+        System.out.println("(" + date + " às " + hour + ") " + sender + " diz: "+ message);
       }
     };
                       //(queue-name, autoAck, consumer);    
-    channel.basicConsume(QUEUE_NAME, true,    consumer);
+    channel.basicConsume(user, true,    consumer);
+    
+
+    System.out.print(">> ");
+    String routing_key = "";
+    String message = scanner.nextLine();
+    while(true){
+      if(message.charAt(0) == '@'){
+        routing_key = new String(message);
+        
+        System.out.print(routing_key + ">> ");
+        message = scanner.nextLine();
+        
+        while(message.charAt(0) != '@'){
+         // try{
+            int ano = LocalDate.getYear();
+            int mes = LocalDate.getMonthValue();
+            int dia = LocalDate.getDayOfMonth();
+
+            String data = dia.toString() + "/" +  mes.toString() + "/" +  ano.toString();
+            Map<String, Object> headers = new HashMap<>();
+            headers.put("sender", user);
+            headers.put("date", data);
+            headers.put("hour", LocalTime.now().toString().substring(0, 5));
+        
+            AMQP.BasicProperties props = new AMQP.BasicProperties.Builder()
+            .headers(headers)
+            .build();
+
+            
+            channel.basicPublish("", routing_key.replace("@", ""), props, message.getBytes("UTF-8"));  
+            System.out.print(routing_key + ">> ");
+            message = scanner.nextLine();
+        //  }catch(Exception e){
+          //  e.printStackTrace();
+          //}
+        }
+      }
+    }
+
   }
 }
